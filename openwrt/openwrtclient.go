33ab8afd32ec31400200e4d26d6df5a0b7475b36
chengli3 <cheng1.li@intel.com>
2020-02-19 22:30:06 +0800

    Add Sdewan Mwan3Conf CRD and controller
    The sdewan operator is developed under kubebuilder framework
    
    We define two CRDs in this patch: Sdewan and Mwan3Conf
    
    Sdewan defines the CNF base info, which node we should deploy the CNF
    on, which network should the CNF use with multus CNI, etc.
    
    The Mwan3Conf defines the mwan3 rules. In the next step, we are going to
    develop the firewall and the ipsec functions. Mwan3Conf is validated by
    k8s api admission webhook.
    
    For each created Sdewan instance, the controller creates a pod, a
    configmap and a service for the instance. The pod runs openswrt which
    provides network services, i.e. sdwan, firewall, ipsec etc.
    
    The configmap stores the network interface information and the
    entrypoint.sh.
    The network interface information has the following format:
    ```
    [
      {
        "name": "ovn-priv-net",
        "isProvider": false,
        "interface": "net0",
        "defaultGateway": false
      }
    ]
    ```
    
    The service created by the controller is used for openwrt api access.
    We call this svc to apply rules, get openwrt info, restart openwrt
    service.
    
    After the openwrt pod ready, the Sdewan controller apply the configured
    mwan3 rules.
    mwan3 rule details are configured in Mwan3Conf CR, which is referenced
    by Sdewan.Spec.Mwan3Conf
    Every time the Mwan3Conf instance changes, the controller re-apply the
    new rules by calling opwnrt
    api. We can also change the rule refernce at the runtime.
    
    Signed-off-by: chengli3 <cheng1.li@intel.com>
    Change-Id: Ic6fa4e8c61da5a560d69f749cd40d8f3b9320e81

---
+++ openwrt/openwrtclient.go
@@ -0,0 +1,148 @@
+package openwrt
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type OpenwrtError struct {
+	Code    int
+	Message string
+}
+
+func (e *OpenwrtError) Error() string {
+	return fmt.Sprintf("Error Code: %d, Error Message: %s", e.Code, e.Message)
+}
+
+type openwrtClient struct {
+	ip       string
+	user     string
+	password string
+	token    string
+}
+
+func NewOpenwrtClient(ip string, user string, password string) *openwrtClient {
+	client := &openwrtClient{
+		ip:       ip,
+		user:     user,
+		password: password,
+		token:    "",
+	}
+
+	return client
+}
+
+// openwrt base URL
+func (o *openwrtClient) getBaseURL() string {
+	return "http://" + o.ip + "/cgi-bin/luci/"
+}
+
+// login to openwrt http server
+func (o *openwrtClient) login() error {
+	client := &http.Client{
+		// block redirect
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	// login
+	login_info := "luci_username=" + o.user + "&luci_password=" + o.password
+	var req_body = []byte(login_info)
+	req, _ := http.NewRequest("POST", o.getBaseURL(), bytes.NewBuffer(req_body))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		return err
+	} else if resp.StatusCode != 302 {
+		// fail to auth
+		return &OpenwrtError{Code: resp.StatusCode, Message: "Unauthorized"}
+	} else {
+		// get token
+		res_cookie := resp.Header["Set-Cookie"][0]
+		res_cookies := strings.Split(res_cookie, ";")
+		for _, cookie := range res_cookies {
+			cookie := strings.TrimSpace(cookie)
+			index := strings.Index(cookie, "=")
+			var key = cookie
+			var value = ""
+			if index != -1 {
+				key = cookie[:index]
+				value = cookie[index+1:]
+			}
+
+			if key == "sysauth" {
+				o.token = value
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+// call openwrt restful API
+func (o *openwrtClient) call(method string, url string, request string) (string, error) {
+	for i := 0; i < 2; i++ {
+		if o.token == "" {
+			err := o.login()
+			if err != nil {
+				return "", err
+			}
+		}
+
+		client := &http.Client{}
+		req_body := bytes.NewBuffer([]byte(request))
+		req, _ := http.NewRequest(method, o.getBaseURL()+url, req_body)
+		req.Header.Add("Cookie", "sysauth="+o.token)
+		resp, err := client.Do(req)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		if resp.StatusCode >= 400 {
+			if resp.StatusCode == 403 {
+				// token expired, retry
+				o.token = ""
+				continue
+			} else {
+				// error request
+				return "", &OpenwrtError{Code: resp.StatusCode, Message: string(body)}
+			}
+		}
+
+		return string(body), nil
+	}
+
+	return "", nil
+}
+
+// call openwrt Get restful API
+func (o *openwrtClient) Get(url string) (string, error) {
+	return o.call("GET", url, "")
+}
+
+// call openwrt restful API
+func (o *openwrtClient) Post(url string, request string) (string, error) {
+	return o.call("POST", url, request)
+}
+
+// call openwrt restful API
+func (o *openwrtClient) Put(url string, request string) (string, error) {
+	return o.call("PUT", url, request)
+}
+
+// call openwrt restful API
+func (o *openwrtClient) Delete(url string) (string, error) {
+	return o.call("DELETE", url, "")
+}
