4ec0171870d0569afdba0d7699900454ceec4652
Ruoyu <ruoyu.ying@intel.com>
2020-05-18 20:37:52 +0800

    Add e2e test scripts for sdewan
    * Add three vagrant vms to setup env
      Three vms are created for edge-a, edge-b and sdewan-hub.
      Each with a separate cluster.
    * Add test scripts for the e2e IPSec scenario
      Establish tunnels between edge and sdewan-hub and test the connections
      between two applications reside in edge-a and edge-b.
    
    Issue-ID: ICN-314
    Change-Id: I0cb8d9d251f0f1cd8ad4c5d58b60e99809c02d0b
    Signed-off-by: Ruoyu <ruoyu.ying@intel.com>

---
+++ platform/test/e2e-test/edge-a/scripts/test-connection.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+test -f /home/vagrant/scripts/variables && . /home/vagrant/scripts/variables
+
+sdewan_virtual_ip=$(kubectl exec -it $sdewan_cnf_name ip address | grep $wan_interface | awk '/inet/{print $2}' | cut -f1 -d "/" | grep 192.168)
+app_pod=$(kubectl get po | grep $app_pod_name | cut -f1 -d " ")
+echo "Logging into the dummy pod $app_pod in edgeA..."
+kubectl exec -it $app_pod bash
+
+
+echo "Determine the ip address of remote host"
+if [ "$sdewan_virtual_ip" == "192.168.1.5" ]
+then 
+	remote_ip="192.168.1.6"
+else
+	remote_ip="192.168.1.5"
+fi
+echo "The remote ip is ${remote_ip}"
+
+echo "Sending request to the remote httpbin..."
+curl -X GET "http://$remote_ip/ip" -H "accept: application/json" -o response.json
+cat response.json
+
+echo "Confirming the testing result..."
+apt install -y jq
+rs=$(jq -r '.origin' response.json)
+if [ "$rs" == "$sdwan_pod_virtual_ip" ]
+then
+	echo "End-to-end test passed"
+else
+	echo "End-to-end test failed. Please check the logs for more details"
---
+++ platform/test/e2e-test/edge-scripts/setup-cnf.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+base=$(pwd)
+
+test -f $base/variables
+. $base/variables
+providerSubnet=${providerSubnet}
+providerGateway=${providerGateway}
+providerExcludeIps=${providerExcludeIps}
+providerNetworkInterface=${providerNetworkInterface}
+ovnSubnet=${ovnSubnet}
+ovnGateway=${ovnGateway}
+ovnExcludeIps=${ovnExcludeIps}
+cnfLan0=${cnfLan0}
+appLan0=${appLan0}
+cnfWanGateway=${cnfWanGateway}
+app_pod_name=${app_pod_name}
+
+clean()
+{
+echo "Cleaning ..."
+kubectl delete -f httpbin-svc.yaml
+kubectl delete -f sdewan-cnf.yaml
+kubectl delete -f network-prepare.yaml
+}
+
+error_detect()
+{
+	echo "Error on line $1"
+	clean
+}
+
+trap "error_detect $LINENO" ERR
+trap "clean" EXIT
+
+echo "Creating ovn networks..."
+cat > network-prepare.yaml << EOF
+---
+apiVersion: k8s.plugin.opnfv.org/v1alpha1
+kind: ProviderNetwork
+metadata:
+  name: pnetwork
+spec:
+  cniType: ovn4nfv
+  ipv4Subnets:
+  - subnet: $providerSubnet
+    name: subnet
+    gateway: $providerGateway
+    excludeIps: $providerExcludeIps
+  providerNetType: DIRECT
+  direct:
+    providerInterfaceName: $providerNetworkInterface
+    directNodeSelector: all
+
+---
+apiVersion: k8s.plugin.opnfv.org/v1alpha1
+kind: Network
+metadata:
+  name: ovn-network
+spec:
+  # Add fields here
+  cniType: ovn4nfv
+  ipv4Subnets:
+  - subnet: $ovnSubnet
+    name: subnet1
+    gateway: $ovnGateway
+    excludeIps: $ovnExcludeIps
+
+EOF
+
+kubectl create -f network-prepare.yaml
+sleep 2
+
+ovnNet=$(kubectl get network | sed -n 2p | awk '{print $1}')
+ovnProviderNet=$(kubectl get providernetwork | sed -n 2p | awk '{print $1}')
+if [ -n "${ovnNet}" ] && [ -n "${ovnProviderNet}" ]
+then
+	echo "Networks created successfully"
+else
+	echo "Networks creation failed"
+	exit 1
+fi
+
+
+echo "Creating sdwan-cnf ..."
+cat > sdewan-cnf.yaml << EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: sdwan-config
+data:
+  entrypoint.sh: |
+    #!/bin/bash
+    # Always exit on errors.
+    set -e
+
+    cat > /etc/config/network << EOF
+    config interface 'lan0'
+        option ifname 'net0'
+        option proto 'static'
+        option ipaddr '$cnfLan0'
+        option netmask '255.255.255.0'
+
+    config interface 'wan0'
+        option ifname 'net1'
+        option proto 'static'
+        option ipaddr '$cnfWan0'
+        option netmask '255.255.255.0'
+    EOF
+    
+    iptables -t nat -L
+    /sbin/procd &
+    /sbin/ubusd &
+    sleep 1
+    /etc/init.d/rpcd start
+    /etc/init.d/dnsmasq start
+    /etc/init.d/network start
+    /etc/init.d/odhcpd start
+    /etc/init.d/uhttpd start
+    /etc/init.d/log start
+    /etc/init.d/dropbear start
+    /etc/init.d/mwan3 restart
+
+    echo "Entering sleep... (success)"
+
+    # Sleep forever.
+    while true; do sleep 100; done
+
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: $sdewan_cnf_name
+  annotations:
+    k8s.v1.cni.cncf.io/networks: '[{ "name": "ovn-networkobj"}]'
+    k8s.plugin.opnfv.org/nfn-network: '{ "type": "ovn4nfv", "interface": [{"name": "ovn-network", "interface": "net0", "ipAddress": "$cnfLan0"}, {"name": "pnetwork", "interface": "net1", "ipAddress": "$cnfWan0", "gateway": "$cnfWanGateway"}]}'
+spec:
+  containers:
+  - name: sdwan-pod-net
+    image: integratedcloudnative/openwrt:test
+    ports:
+      - containerPort: 22
+      - containerPort: 80
+    command:
+    - /bin/sh
+    - /init/entrypoint.sh
+    imagePullPolicy: IfNotPresent
+    securityContext:
+      privileged: true
+    volumeMounts:
+      - name: entrypoint-sh
+        mountPath: /init
+  volumes:
+    - name: entrypoint-sh
+      configMap:
+        name: sdwan-config
+        items:
+        - key: entrypoint.sh
+          path: entrypoint.sh
+EOF
+
+kubectl create -f sdewan-cnf.yaml
+sleep 20
+
+sdwan_status=$(kubectl get po | grep $sdewan_cnf_name | awk '{print $3}')
+if [ "$sdwan_status" == "Running" ]
+then
+	echo "Sdewan cnf $sdewan_cnf_name created successfully"
+else
+        sleep 40
+	sdwan_status=$(kubectl get po | grep $sdewan_cnf_name | awk '{print $3}')
+	if [ "$sdwan_status" != "Running" ]
+	then
+	     echo "Sdewan cnf creation failed"
+             exit 2
+        fi
+fi
+
+kubectl exec -it  $sdewan_cnf_name /etc/init.d/network restart
+
+
+cat > httpbin-svc.yaml << EOF
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: my-http-service
+spec:
+  selector:
+    app: MyApp
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080
+
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $app_pod_name
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      name: simple-http-service
+  template:
+    metadata:
+      labels:
+        name: simple-http-service
+        app: MyApp
+      annotations:
+        k8s.v1.cni.cncf.io/networks: '[{ "name": "ovn-networkobj"}]'
+        k8s.plugin.opnfv.org/nfn-network: '{ "type": "ovn4nfv", "interface": [{"name": "ovn-network", "interface": "net0", "ipAddress": "$appLan0"}]}'
+    spec:
+      containers:
+        - name: simple-http-service
+          image: integratedcloudnative/httpbin:test
+          ports:
+            - containerPort: 80
+          imagePullPolicy: IfNotPresent
+          securityContext:
+                  privileged: true
+EOF
+kubectl create -f httpbin-svc.yaml
+sleep 20
+
+appStatus=$(kubectl get po | grep simple-http-service | awk '{print $3}')
+if [ "$appStatus" == "Running" ] 
+then
+	echo "Application $app_pod_name installation success"
+else
+        sleep 20
+        appStatus=$(kubectl get po | grep simple-http-service | awk '{print $3}')
+        if [ "$appStatus" != "Running" ]
+        then
+             echo "Application creation failed"
+             exit 1
+        fi
+fi
+
+echo "Setup openwrt configurations"
+bash sdwan_verifier.sh
