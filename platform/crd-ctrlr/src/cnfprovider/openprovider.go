7d6b77198f636eece0eb462a96be1fa272051493
Yao Le <le.yao@intel.com>
2020-05-29 07:01:37 +0000

    Add missing errors messages
    Report the 'Pod no IP' issues
    
    Signed-off-by: Yao Le <le.yao@intel.com>
    Change-Id: Ieaf2997ca0ed1bd1f167d9d5f8c77cb43ab3fb45

--- platform/crd-ctrlr/src/cnfprovider/openprovider.go
+++ platform/crd-ctrlr/src/cnfprovider/openprovider.go
@@ -52,6 +52,10 @@ func (p *OpenWrtProvider) AddOrUpdateObject(handler basehandler.ISdewanHandler, 
 	}
 	cnfChanged := false
 	for _, pod := range podList.Items {
+		if pod.Status.PodIP == "" {
+                        return false, errors.New("Pod no IP")
+                }
+		
 		clientInfo := &openwrt.OpenwrtClientInfo{Ip: pod.Status.PodIP, User: "root", Password: ""}
 		runtime_instance, err := handler.GetObject(clientInfo, new_instance.GetName())
 		changed := false
