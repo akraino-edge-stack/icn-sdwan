68fb4f253575c4d6b318d6e8c580e56f62c4b8fc
Yao Le <le.yao@intel.com>
2020-06-05 00:13:27 +0800

    Add watch for CR and CNF
    Watch the CNF status and push the related CR requests to queue.
    
    Signed-off-by: Yao Le <le.yao@intel.com>
    Change-Id: Id3adaf68b860efefdb00ffe5620aef11b9aa787f

--- platform/crd-ctrlr/src/controllers/mwan3policy_controller.go
+++ platform/crd-ctrlr/src/controllers/mwan3policy_controller.go
@@ -27,6 +27,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+        "sigs.k8s.io/controller-runtime/pkg/source"
 
 	batchv1alpha1 "sdewan.akraino.org/sdewan/api/v1alpha1"
 	"sdewan.akraino.org/sdewan/openwrt"
@@ -130,5 +132,11 @@ func (r *Mwan3PolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	ps := builder.WithPredicates(predicate.GenerationChangedPredicate{})
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&batchv1alpha1.Mwan3Policy{}, ps).
+		Watches(
+                        &source.Kind{Type: &appsv1.Deployment{}},
+                        &handler.EnqueueRequestsFromMapFunc{
+                                ToRequests: handler.ToRequestsFunc(GetToRequestsFunc(r, &batchv1alpha1.Mwan3PolicyList{})),
+                        },
+                        Filter).
 		Complete(r)
 }
