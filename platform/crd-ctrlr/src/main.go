41d402126a32eaf6b056b2bd54b00134ad0c5b76
Le Yao <le.yao@intel.com>
2020-11-23 06:15:36 +0000

    Application CR implemention
    Implement Application CR and CR controller.
    Call Application RESTful API to add rules for Application deployment
    
    Signed-off-by: Le Yao <le.yao@intel.com>
    Change-Id: I425f431bea20f372dc95cde1439259ad29e93773

--- platform/crd-ctrlr/src/main.go
+++ platform/crd-ctrlr/src/main.go
@@ -217,6 +217,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CNFService")
 		os.Exit(1)
 	}
+	if err = (&controllers.SdewanApplicationReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SdewanApplication"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SdewanApplication")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
