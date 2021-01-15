722b64b911187de95a96edb87255226c3751fe0d
Le Yao <le.yao@intel.com>
2021-01-15 07:32:23 +0000

    Implement Route CR controller
    The Route CR controller handles the Route resource and call CNF Route
    RESTful API to add/update/delete/get the route rule.
    
    Signed-off-by: Le Yao <le.yao@intel.com>
    Change-Id: I95871e1e95ccacf5790697d2e5105448f630aac2

--- platform/crd-ctrlr/src/main.go
+++ platform/crd-ctrlr/src/main.go
@@ -229,6 +229,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SdewanApplication")
 		os.Exit(1)
 	}
+	if err = (&controllers.CNFRouteReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("CNFRoute"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CNFRoute")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("start CNFStatusController to query CNF status periodicly")
